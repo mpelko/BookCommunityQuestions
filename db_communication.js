04edb2a68269bdf332a5d10819cf5b9ecfba03ec
Miha Pelko <mpelko@gmail.com>
2013-04-06 05:19:22 +0100

    database interface to frontent finished along with tests and thin database.

--- db_communication.js
+++ db_communication.js
@@ -4,9 +4,45 @@
 
 //function returns all hte questions and answers from a book
 //TODO: implement some sort of cashing, so we don't need all the qa every time.
+
+function get_all_QA_html()
+{
+ return []   
+}
+
+
+function test_QA_get_all(book_id)
+{
+    QAs = []
+    for (var i = 0; i < test_QA_all.length; i++) 
+    {
+        Q = test_QA_all[i];
+        if(Q.book==book_id)
+        {
+            QAs.push(Q);
+        };
+    };    
+    return QAs
+}
+
+function send_answer_test(answer, question, book_id)
+{
+    for (var i = 0; i < test_QA_all.length; i++) 
+    {
+        Q = test_QA_all[i];
+        if(Q.book==book_id && Q.title==question)
+        {
+            Q.answers.push(answer);
+        };
+    };        
+}
+
 function get_all_QA(book_id)
 {
-    if (DEBUG){return test_QA_all;}
+    if (DEBUG)
+    {
+        return test_QA_get_all(book_id);
+    }
     else {
         if(using_Forum_asDB){
             return get_QA_all_forum(book_id);
@@ -17,10 +53,31 @@ function get_all_QA(book_id)
     };
 }
 
+
+function send_answer(answer, question, book_id)
+{
+    if (DEBUG)
+    {
+        send_answer_test(answer, question, book_id);
+    }
+    else {
+        if(using_Forum_asDB){
+            send_answer_forum(question, book_id);
+        }
+        else{
+            send_answer_DB(question, book_id);
+        };
+    };    
+    
+}
+
 //wrapper function for stroing the q in the db
 function send_question(question, book_id, location)
 {
-    if (DEBUG){return;}
+    if (DEBUG)
+    {
+        test_QA_all.push({"book":book_id, "title":question, "location":location, "answers":[]})
+    }
     else {
         if(using_Forum_asDB){
             send_question_forum(question, book_id, location);
@@ -45,3 +102,52 @@ function get_QA_subset(book_id, min_loc, max_loc)
     };
     return subset;
 }
+
+function get_the_right_QA(book_id, location)
+{
+    all_QA = get_all_QA(book_id);
+    closest_Q = {"location": location+1000}
+    for (var i = 0; i < all_QA.length; i++) 
+    {
+        Q = all_QA[i];
+        if(!(Q.location<location) && (Q.location-location)<closest_Q.location)
+        {
+            closest_Q = Q;
+        };
+    };
+    if(closest_Q.location-location < 200)
+    {
+        return closest_Q
+    } 
+    else 
+    {
+        return {}    
+    }
+}
+
+function get_the_html_question(book_id, location)
+{
+    Q = get_the_right_QA(book_id, location)
+    
+    if(!("title" in Q))
+    {
+        return "";
+    }
+    else
+    {
+        html = "<h4>"
+        html = html.concat(Q.title,"</h4>")
+        if(Q.answers.length > 0)
+        {
+            html = html.concat("<ul>")
+            for (var i = 0; i < Q.answers.length; i++) 
+            {
+                A = Q.answers[i];
+                html = html.concat("<il>", A, "</il>")
+            };
+            html = html.concat("</ul>")
+        };
+        return html
+    };
+}
+
--- globals.js
+++ globals.js
@@ -1,3 +1,5 @@
 var using_Forum_asDB=false
 var DEBUG=true
-var test_QA_all=[{"title": "q1text", "location":100, "answers": ["a1", "a2"]}]
+var test_QA_all=[{"book": "Book1", "title": "q1text", "location":100, "answers": ["a1", "a2"]}, 
+                 {"book": "Book1", "title": "q2text", "location":1300, "answers": ["aaa1", "aaaa2"]}]
+var all_QA = "33"
