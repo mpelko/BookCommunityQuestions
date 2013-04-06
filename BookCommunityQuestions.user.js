08e070d2934c6517c3111a22f631669848b13be1
Scott Lowe <scottlowe0@gmail.com>
2013-04-06 05:21:17 +0100

    Added lots of font-end functions

--- BookCommunityQuestions.user.js
+++ BookCommunityQuestions.user.js
@@ -7,12 +7,6 @@
 // @run-at        document-end
 // @grant         none
 // @require globals.js
-// @require callalerttest.js
-// @require popupform.js
-// @require fg_ajax.js
-// @require fg_form_submitter.js
-// @require fg_moveable_popup.js
-// @require gen_validatorv31.js
 // @require db_communication.js
 // ==/UserScript==
 // Notes:
@@ -28,10 +22,235 @@ function get_read_loc() {
     return loc;
 }
 
-function greasemonkey_main() {
-   loc = get_read_loc()
-   alert(loc);
+function getBookName() {
+    // <label id="kindleReader_title" style="color: rgb(153, 153, 153);">Les Misérables - Tome I - Fantine</label>
+    var doc2=document.getElementById("KindleReaderIFrame").contentDocument;
+    var book = doc2.getElementById("kindleReader_title").innerHTML;
+    return book;
 }
 
-setTimeout(greasemonkey_main, 4000);
+function greasemonkey_main() {
+    //loc = get_read_loc();
+    //book = getBookName();
+    //alert(book + ': at ' + loc);
+    setup_everything();
+}
+setTimeout(greasemonkey_main, 2000);
+
+
+function setup_everything() {
+//alert('setting up all');
+    addPoseForm();
+    setTimeout(setup_newels, 2000);
+    setTimeout(setup_loopers, 2000);
+}
+
+function setup_newels() {
+//alert('setting up new elements');
+    addPoseButton();
+    make_panel();
+    add_qmark_button();
+}
+
+function setup_loopers() {
+//alert('adding loopers');
+    var loopersrunner = window.setInterval(run_loopers,100);
+}
+
+function run_loopers() {
+//alert('running loopers');
+    refreshPoseButton();
+}
+
+function refreshPoseButton() {
+//alert('refreshing pose button');
+    doc2=document.getElementById("KindleReaderIFrame").contentDocument;
+    var myElem = doc2.getElementById('bcq_hoverbutton');
+    if (myElem == null) {
+//alert('had to add pose button');
+        addPoseButton();
+    }
+    else {
+//alert('reset pose class');
+        myElem.className = "kindle_menu_button button_enabled";
+    }
+}
+
+function addPoseButton() {
+//alert('adding pose button');
+    doc2=document.getElementById("KindleReaderIFrame").contentDocument;
+    if (doc2 == null) {
+//        alert('No doc2');
+        return false;
+    }
+    var popupmenu = doc2.getElementById('kindle_menu_border');
+    if (popupmenu == null) {
+//        alert('No kindle_menu_border');
+        return false;
+    }
+    var newsep=document.createElement("div");
+    newsep.className = "kindle_menu_separator"
+    newsep.style = "display: none;"
+    popupmenu.appendChild(newsep);
+    var newbutton = document.createElement("div");
+    newbutton.id = 'bcq_hoverbutton';
+    newbutton.className="kindle_menu_button button_enabled";
+    newbutton.addEventListener('click',showPoseForm);
+    var newbuttontext = document.createTextNode("Pose question");
+    newbutton.appendChild(newbuttontext);
+    popupmenu.appendChild(newbutton);
+}
+
+
+function addPoseForm() {
+//alert('adding pose form');
+//doc2=document.getElementById("KindleReaderIFrame").contentDocument;
+document.body.innerHTML = '<div id="bcq_bgmask" style="position:absolute;left:0px;top:0px;height:100%;width:100%;background:rgba(100,100,100,0.5);z-index:10000;display:none;"><<div id="bcq_overform" style="border-radius: 15px 15px 15px 15px; z-index: 10000; top: 20%; left: 20%; right: 20%; bottom: 20%; background: none repeat scroll 0% 0% rgb(250, 250, 250); border: 10px solid rgb(51, 51, 51); padding: 30px; position: absolute;"><form onsubmit="return submitPoseForm()"><p>Submit your question about this book: <i>' + getBookName() + '</i></p><p>Question:</p><textarea name="question-title" cols="60" rows="10" id="bcq_qsubmitinput"></textarea><br/><input type="submit" name="submit" value="Post Question"/><button type="button" onclick="hidePoseForm()">Cancel</button><form></div></div>' + document.body.innerHTML;
+// Add button to close this panel
+// Add box for entering email address?
+}
+
+
+function showPoseForm() {
+    document.getElementById('bcq_bgmask').style.display = 'block';
+}
+
+function hidePoseForm() {
+    document.getElementById('bcq_bgmask').style.display = 'none';
+}
+
+function submitPoseForm() {
+    var loc = get_read_loc();
+    var qtext = document.getElementById('bcq_qsubmitinput').value;
+    var book = getBookName();
+// Do something with this to submit it
+// Check if submission is successful
+// Tell the user whether or not successful
+// Hide the submission box if appropriate
+hidePoseForm();
+// Don't refresh the page
+return false;
+}
+
+
+/////////////////////////////////
+
+function make_panel() {
+//alert('making panel');
+
+   document.body.style.background = "white";
+
+   // Create the question content panel.
+   var container = document.getElementById("KindleReaderContainer");
+   var qcontent = document.createElement("div");
+   var qtextarea = document.createElement("textarea");
+   var qforum = document.createElement("div");
+   
+   qforum.id = "question_forum";
+   qtextarea.rows = "3";
+   qtextarea.cols = "27";
+   qtextarea.id = "question_textarea";
+   qtextarea.style.postition = "absolute";
+   qtextarea.style.top = "300px";
+   qtextarea.style.right = "70px";
+   
+   qcontent.id = "question_content";
+   qcontent.style.float = "left";
+   qcontent.style.width = "30%";
+   qcontent.isout = "False";
+   container.appendChild(qcontent);
+   qcontent.appendChild(qforum);
+   qcontent.appendChild(qtextarea);
+
+
+<div id="question_content" style="width: 250px; right: 70px; top: 70px; position: absolute; background: none repeat scroll 0% 0% white; display: block;">
+<div id="question_forum">
+<textarea id="question_textarea" rows="3" cols="27" style="top: 300px; right: 70px; width: 30%; height: 90px;"></textarea>
+</div>
+   
+}
+
+
+function remove_panel() {
+
+    // Get the question content panel.
+    var content = document.getElementById("KindleReaderContainer");
+    var qcontent = document.getElementById("question_content");
+
+    // Give the panel dimensions.
+    qcontent.style.right = "70px";
+    qcontent.style.top = "70px";
+    qcontent.style.position = "absolute";
+    qcontent.style.width = "0";
+    qcontent.style.display = "none";
+
+    // Reduce size of content panel.
+    var frame = document.getElementById("KindleReaderIFrame");
+    frame.style.width = "100%";   
+
+}
+
+function add_panel() {
+
+   // Get the question content panel.
+   var content = document.getElementById("KindleReaderContainer");
+   var qcontent = document.getElementById("question_content");
+   var qforum = document.getElementById("question_forum");
+
+   // Give the panel dimensions.
+   qcontent.style.right = "70px";
+   qcontent.style.top = "70px";
+   qcontent.style.position = "absolute";    
+   qcontent.style.width = "250px";
+   qcontent.style.background = "white";
+   qcontent.style.display = "block";
+   // Sample text.
+   qforum.innerHTML = "Portland street art dreamcatcher, 3 wolf moon synth banjo american apparel gentrify. Blog ullamco echo park, selfies wes anderson vegan bespoke. Wes anderson iphone cosby sweater, deep v trust fund biodiesel odd future irure twee skateboard. Vero single-origin coffee mcsweeney's next level, high life seitan tousled messenger bag deep v. Marfa gluten-free yr incididunt vinyl, mcsweeney's cosby sweater. Intelligentsia vegan aesthetic, dreamcatcher fashion axe fixie qui. Seitan flannel etsy Austin sint food truck.<br><br>";
+
+   // Reduce size of content panel.
+   var frame = document.getElementById("KindleReaderIFrame");
+   frame.style.width = "70%";
+}
+
+
+function toggle_panel() {
+
+   var qcontent = document.getElementById("question_content");
+   if (qcontent.isout == "True") {
+       remove_panel();
+       qcontent.isout = "False";
+   }
+   else {
+       add_panel();
+       qcontent.isout = "True";
+   }
+}
+
+///////////////////////////
+
+function add_qmark_button() {
+//alert('adding qmark');
+
+    doc2=document.getElementById("KindleReaderIFrame").contentDocument;
+    topmenu=doc2.getElementById("readerControlsLeft");
+    var qbutton = document.createElement("div");
+    qbutton.id = "bcq_qbutton";
+    qbutton.className="header_bar_icon";
+    var qicon = document.createElement("img");
+    qicon.src = "https://s3-eu-west-1.amazonaws.com/amazonhack/qbuttonTr.png"
+    qicon.height = "25";
+    qicon.width="16";
+    qicon.type = "image";
+    qicon.addEventListener('mouseout', function() {
+       this.src='https://s3-eu-west-1.amazonaws.com/amazonhack/qbuttonTr.png'
+    } );
+    qicon.addEventListener('mouseover', function() {
+       this.src='https://s3-eu-west-1.amazonaws.com/amazonhack/qbuttonTrBl.png'
+    } );
+    qicon.addEventListener('click', toggle_panel);
+    qbutton.appendChild(qicon);
+    topmenu.appendChild(qbutton);
+}
+
+
 
