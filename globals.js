1913ddfd491868def079325790ebff0be023be58
ACPrice <andrew.charles.price@gmail.com>
2013-04-06 08:08:31 +0200

    Update globals.js

--- globals.js
+++ globals.js
@@ -1,5 +1,11 @@
 var using_Forum_asDB=false
 var DEBUG=true
-var test_QA_all=[{"book": "Book1", "title": "q1text", "location":100, "answers": ["a1", "a2"]}, 
-                 {"book": "Book1", "title": "q2text", "location":1300, "answers": ["aaa1", "aaaa2"]}]
+var test_QA_all=[{"book": "Sidelights on Relativity", "title": "Wait, how are the properties of light similar to those of waves?", "location":26, "answers": ["Light reflects in the same manner as a wave would.", "Light exhibits the Doppler effect."]},
+                 {"book": "Sidelights on Relativity", "title": "What's the ether?", "location":35, "answers": ["James Maxwell thought light moved through a medium called the ether. He was wrong.", "The Michelson-Morley experiment was the first strong evidence against the ether theory."]}
+                 {"book": "Sidelights on Relativity", "title": "What's so special about the special theory of relativity?", "location":73, "answers": ["The special theory of relativity postulates that all motion is relative and the speed of light is constant in all refernce frames."]}
+                 {"book": "Sidelights on Relativity", "title": "How is general relativity different from special relativity?", "location":147, "answers": ["General relativity combines special relativity with Einstein's theory of gravity as the curvature of space-time."]}
+                 {"book": "Sidelights on Relativity", "title": "Is how the flux capacitor works?", "location":233, "answers": ["No. The flux capacitor is not real. It only exists in Back to the Future.", "I think so."]}
+                 {"book": "Sidelights on Relativity", "title": "Who should I read on epistemology?", "location":279, "answers": ["Renee Descartes.", "More modern: Peter Unger."]}
+                 {"book": "Sidelights on Relativity", "title": "So is a moebius strip finite but unbounded?", "location":299, "answers": ["Yes, it's one-dimensional, finite, unbounded."]}]
+                 
 var all_QA = "33"
