b0b10c44b06039553d1c2155903c345dbf608c10
Catalina Predoi <catalina.predoi@gmail.com>
2013-04-15 20:00:58 +0100

    Added rank attr

--- application.py
+++ application.py
@@ -211,6 +211,7 @@ def addAnswer(environ, start_response):
                 attrs={"answerID": answerID,
                     "answer": answer,
                     "questionID": questionID,
+                    "rank": 0, 
                     "username": username} )
             conn.put_item(item)
             if 'callback' in parameters:
@@ -299,7 +300,7 @@ def getAnswersforQ(conn, questionID):
     jsonlist = []
     answers = conn.scan(qtable, scan_filter={'questionID': condition.EQ(questionID)})
     for answer in answers:
-        jsondict = {"answerID":answer["answerID"], "text":answer["answer"], "username":answer["username"]}
+        jsondict = {"answerID":answer["answerID"], "text":answer["answer"], "rank":answer["rank"], "username":answer["username"]}
         jsonlist.append(jsondict)
     return jsonlist
     
@@ -316,7 +317,7 @@ def getAnswers(environ, start_response):
         jsonlist = []
         answers = conn.scan(qtable, scan_filter={'questionID': condition.EQ(questionID)})
         for answer in answers:
-            jsondict = {"answerID":answer["answerID"], "answer":answer["answer"], "username":answer["username"]}
+            jsondict = {"answerID":answer["answerID"], "answer":answer["answer"], "rank":answer["rank"], "username":answer["username"]}
             jsonlist.append(jsondict)
         response = json.dumps(jsonlist)
         if 'callback' in parameters:
