770c5a40e89f2e71c8466e4798e389a5ab542f68
Scott Lowe <scottlowe0@gmail.com>
2013-04-15 22:21:58 +0100

    better blue goto

--- styles.js
+++ styles.js
@@ -34,7 +34,7 @@ addGlobalStyle('#question_forum {margin:auto;padding:15px;color:white;} #questio
 addGlobalStyle('.bcq_q h4 {margin:6px 0px;} .bcq_q ul {margin:0 0 4px;padding-left:20px;} .bcq_q_inner {margin-left:20px;margin-bottom:15px;}');
 
 // GoTo button
-addGlobalStyle('.bcq_goto {font-size:small;color:blue;display:block;magin:auto;} .bcq_goto:hover {text-decoration:underline;}');
+addGlobalStyle('.bcq_goto {font-size:small;color:#88f;display:block;magin:auto;} .bcq_goto:hover {text-decoration:underline;}');
 
 // Question expansion
 //.bcq_hcontracted {display:block;max-height:40px;text-overflow:ellipsis;} .bcq_hexpanded {display:inline;}
